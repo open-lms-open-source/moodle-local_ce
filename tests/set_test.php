3e5ca1fef3cacf650db80d3b9d4e2ca7330343f6
Jonathan Garcia <jonathan.garcia@openlms.net>
2021-02-17 16:06:13 -0500

    INT-16757: Fix cache error on upgrade path and reapply INT-11037 from lib/behat/behat_base.php, setUp functions fixed and deprecated messages

--- tests/set_test.php
+++ tests/set_test.php
@@ -33,7 +33,7 @@ defined('MOODLE_INTERNAL') || die();
  */
 class local_ce_set_testcase extends advanced_testcase {
 
-    public function setUp() {
+    public function setUp(): void {
         $this->resetAfterTest();
     }
 
