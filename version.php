cb1fe6dc8cd6dfce16d842804fd0a32490b10814
Bryan Cruz <bryan.cruz@openlms.net>
2023-04-11 17:08:30 +0000

    INT-18711: 4.0 MP2 Version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2023012400;
-$plugin->release = '4.0.5';
+$plugin->version  = 2023041100;
+$plugin->release = '4.0.7';
 $plugin->requires = 2022041900;
 $plugin->component = 'local_ce';
 $plugin->cron = 0;
